4728029b4d3232caa0ec8a2fdedfc57d083a2b06
Liam Chalk <lchalk@hmc.edu>
2021-03-06 15:02:22 -0500

    Dilution rendered

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # BioCalculator
 Creating a calculator for common molecular biology lab experiments.
+
+So far:
+- Implemented a simple concentration change calculator!
+- Able to do simple dilution/concentration increase; with rendering in Django
+
+
+## Acknowledgement
+Developers:
+Tom Fu (@tommyfuu)
+Lucy Paddock (@lucinda-paddock)
+Liam Chalk (@liamchalk00)
+April Zhao (@abooli)
+
+Affiliation: Harvey Mudd College BioMakerspace (Polymerspace).
+Faculty advisor: Dr. Dan Stoebel.
+
+This project is part of the intellectual property of the Harvey Mudd College BioMakerspace. Usage of this repository should be directed according to the MIT License. The funding support of the Harvey Mudd College BioMakerspace comes from Harvey Mudd College Office of Community Engagement and the college's Shanahan Fund.
--- homepage/views.py
+++ homepage/views.py
@@ -21,7 +21,14 @@ def about(request):
 
 # CALCULATORS
 # DILUTION CALCULATOR
-
+### GLOBAL VARIABLES
+INPUTVOL = None
+INPUTCONC = None
+INPUTSOLUTE = None
+FINALVOL = None
+FINALCONC = None
+ADDEDSOLUTE = None
+ADDEDWATER = None
 
 def dilution_input_view(request):
     # if this is a POST request we need to process the form data
@@ -37,15 +44,21 @@ def dilution_input_view(request):
             finalConc = form.cleaned_data['FINALCONC']
             addedSoluteVol = form.cleaned_data['ADDEDSOLUTE']
             waterVol = form.cleaned_data['ADDEDWATER']
-            inputVol, inputConc, inputSolute, finalVol, finalConc, addedSoluteVol, addedWater = changeConcentrationTable(
-                inputVol, inputConc, finalVol, finalConc, inputSolute, addedSoluteVol)
-            print("XSCSXSCACAXAS")
-            print(inputVol, inputConc, inputSolute, finalVol,
-                  finalConc, addedSoluteVol, addedWater)
+            # inputVol, inputConc, inputSolute, finalVol, finalConc, addedSoluteVol, addedWater = changeConcentrationTable(
+            #     inputVol, inputConc, finalVol, finalConc, inputSolute, addedSoluteVol)
+            # INPUTVOL = inputVol
+            # INPUTCONC = inputConc
+            # INPUTSOLUTE = inputSolute
+            # FINALCONC = finalConc
+
+            INPUTVOL, INPUTCONC, INPUTSOLUTE, FINALVOL, FINALCONC, ADDEDSOLUTE, ADDEDWATER = changeConcentrationTable(inputVol, inputConc, finalVol, finalConc, inputSolute, addedSoluteVol)
+            print("Here are the calculated input values for your desired output:")
+            # print(c)
             # print(inputVol, inputConc, inputSolute, finalVol,
             #       finalConc, addedSoluteVol, addedWater)
             # redirect to a new URL:
-            return HttpResponseRedirect('http://127.0.0.1:8000/dilutionCalculatorResult')
+            # return HttpResponseRedirect('http://127.0.0.1:8000/dilutionCalculatorResult')
+            return render(request, 'concentrationCalcResult.html', {"inputVol": INPUTVOL, "inputConc": INPUTCONC, "inputSolute": INPUTSOLUTE, "finalVol": FINALVOL, "finalConc": FINALCONC, "addedSolute": ADDEDSOLUTE, "addedWater": ADDEDWATER})
 
     # if a GET (or any other method) we'll create a blank form
     else:
@@ -55,4 +68,4 @@ def dilution_input_view(request):
 
 def dilution_result_view(request):
     # return HttpResponse("Contact page!")
-    return render(request, 'concentrationCalcResult.html', {})
+    return render(request, 'concentrationCalcResult.html', {"inputVol": INPUTVOL, "inputConc": INPUTCONC, "inputSolute": INPUTSOLUTE, "finalVol": FINALVOL, "finalConc": FINALCONC, "addedSolute": ADDEDSOLUTE, "addedWater": ADDEDWATER})
